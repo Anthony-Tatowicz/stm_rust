d23303c1fa0a289b0683d9470433326ace392ff0
Anthony Tatowicz <ajtatowicz@gmail.com>
2019-03-23 22:45:56 -0700

    Initial working project

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,11 @@ extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to c
 // extern crate panic_itm; // logs messages over ITM; requires ITM support
 // extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
 
-use cortex_m::asm;
 use cortex_m_rt::entry;
+use stm32f30x;
 
 #[entry]
-fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+fn main() -> ! { 
 
     loop {
         // your code goes here
